48cad1917ef5391aa800ac7aecf117059ba49954
Fused3195 <fusedrenegade@gmail.com>
2020-08-20 18:23:34 -0400

    Create Hero

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from 'react-bootstrap/Button';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {Button, Container, Jumbotron, Row, Col}  from 'react-bootstrap'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+        <section className="home">
+          <Container fluid>
+              <Row>
+                <Col className="hero-col">
+                  <Jumbotron className="hero">
+                    <Container className="hero-text">
+                      <h1 className="greeting">Hi! I'm James!</h1>
+                      <p>
+                        "Some type of quote" - somebody
+                      </p>
+                    </Container>
+                  </Jumbotron>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+          <section className="about">
 
-        <Button variant="primary">Primary</Button>
-      </header>
+          </section>
+          <section className="portfolio">
+            
+            </section>
     </div>
   );
 }
